aa49bd44eb622fd5ba39bc0c9fd800ea410dc614
Edward Mendoza <lalo.garibo90@gmail.com>
2020-05-15 15:51:43 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,28 @@
-// create your App component here
+import React, { Component } from "react";
+
+export default class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      teams: [],
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>App component</h1>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    fetch("http://api.open-notify.org/astros.json")
+      .then((data) => data.json())
+      .then((teams) => {
+        console.log(teams);
+        this.setState({
+          teams,
+        });
+      });
+  }
+}
